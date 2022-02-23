587dbfc95b3d66caf18b584b969239a8887e94b4
venkatjan25 <98408446+venkatjan25@users.noreply.github.com>
2022-02-23 15:26:25 +0530

    added files

---
+++ src/main/java/com/example/demo/dto/Details.java
@@ -0,0 +1,65 @@
+package com.example.demo.dto;
+
+
+import java.util.Objects;
+
+
+public class Details {
+
+    private int userId;
+    private int id;
+    private String title;
+    private String body;
+
+
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Details other = (Details) obj;
+        return Objects.equals(id, other.id);
+    }
+
+}
