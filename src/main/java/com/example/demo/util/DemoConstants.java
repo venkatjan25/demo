7a86b55c8a8e7985b35ee4c0b9bee01a8497238d
venkatjan25 <98408446+venkatjan25@users.noreply.github.com>
2022-02-23 15:27:25 +0530

    added files

---
+++ src/main/java/com/example/demo/util/DemoConstants.java
@@ -0,0 +1,7 @@
+package com.example.demo.util;
+
+public class DemoConstants {
+
+    public static final String BASE_URI= "http://jsonplaceholder.typicode.com/posts";
+    public static final String UPDATE_URI = "https://jsonplaceholder.typicode.com/posts/4";
+}
