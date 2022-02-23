4dd8d0720b83830e3675b05694a8801360980b9b
venkatjan25 <98408446+venkatjan25@users.noreply.github.com>
2022-02-23 15:23:22 +0530

    added files

---
+++ src/main/java/com/example/demo/service/DetailsService.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.DetailsDAO;
+import com.example.demo.dto.Details;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class DetailsService {
+
+    @Autowired
+    private DetailsDAO detailsDAO;
+    public Long getDetails(){
+        return detailsDAO.getDetails();
+    }
+    public Details getUpdatedDetails(){
+       return detailsDAO.getUpdatedDetails();
+    }
+}
