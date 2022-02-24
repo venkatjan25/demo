f6656b5253d2dbccbc5258e46d6435a6d96fdaec
venkatjan25 <98408446+venkatjan25@users.noreply.github.com>
2022-02-24 13:15:00 +0530

    Added files

--- src/test/java/com/example/demo/controller/DetailsControllerTest.java
+++ src/test/java/com/example/demo/controller/DetailsControllerTest.java
@@ -1,29 +1,48 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.Details;
+import com.example.demo.service.DetailsService;
 import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import static org.mockito.Mockito.when;
+
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-@SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
-public class DetailsControllerTest extends TestCase {
-    @Autowired
+import static org.mockito.MockitoAnnotations.initMocks;
+@RunWith(MockitoJUnitRunner.class)
+public class DetailsControllerTest {
+
+    @InjectMocks
     private DetailsController detailsController;
+    @Mock
+    private DetailsService detailsService;
 
+    @Before
+    public void init() {
+        initMocks(this);
+    }
     @Test
     public void countUserIdTest(){
+        Long count = 10L;
+        when(detailsService.getDetails()).thenReturn(count);
         Long totalRecords = detailsController.getDetails();
-        assertTrue(totalRecords>0);
+        Assert.assertEquals(count,totalRecords);
     }
 
 
     @Test
     public void updateTitle(){
-        Details details = detailsController.getUpdatedDetails();
-        assertNotNull(details);
+        Details d = new Details();
+        d.setTitle("1800floweres");
+        d.setBody("1800floweres");
+
+        when(detailsService.updateUserDetails(4,d)).thenReturn(d);
+        Details details = detailsController.updateUserDetails(4,d);
+        Assert.assertEquals(d,details);
     }
 }
